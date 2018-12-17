2399685eadbcc7f84dc9e7ed036c7de1b3cc671a
Lupus <bertram@lupusbytes.com>
2018-12-17 03:55:55 +0100

    public/thread.js: added html generation submit function

--- public/js/thread.js
+++ public/js/thread.js
@@ -1,13 +1,36 @@
+
 // In JavaScript, 'replace()' only replaces the first occurence ....
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
+
+
+function addPostHtml(post) {
+    const html = `
+    <div class="col-2 user-col rounded text-center">
+        <strong>` + post.creator.username + `</strong><br><br>
+        <img src="/images/default-user.png" width="100" height="100">
+        <br><br><br>
+        <p>Medlem siden: ` + new Date(post.creator.created_at).toLocaleDateString('da-DK') + `</p>
+        <p class="tiny">Sidst aktiv: ` + new Date(post.creator.last_activity).toLocaleString('da-DK') + `</p>
+    </div>
+    <div class="col-10 post-col rounded">
+        <p>` + replaceAll(post.content, "\n", "<br>") + `</p>
+        <div class="bottom-right-text tiny">
+        Oprettet ` + new Date(post.creator.created_at).toLocaleString('da-DK') + `</div>
+    </div>`
+    $("#post-row").append(html);
+}
+
+
 $(document).ready(function () {
     const pathname = window.location.pathname;
     const paths = pathname.split('/');
     const categoryId = paths[3];
     const threadId = paths[5];
-    const postsUrl = "/api/categories/" + categoryId + "/threads/" + threadId + "/posts"
+    const postsUrl = "/api/categories/" + categoryId + "/threads/" + threadId + "/posts";
+
+    
     $.ajax({
         type: "GET",
         url: postsUrl,
@@ -15,48 +38,33 @@ $(document).ready(function () {
 
             const threadName = data.data.name;
             $("#thread-title").append(threadName);
-
             $.each(data.data.posts, function (key, value) {
-                const html = `
-            <div class="col-2 user-col rounded text-center">
-                <strong>` + value.creator.username + `</strong><br><br>
-                <img src="/images/default-user.png" width="100" height="100">
-                <br><br><br>
-                <p>Medlem siden ` + new Date(value.creator.created_at).toLocaleDateString('da-DK') + `</p>
-            </div>
-            <div class="col-10 post-col rounded">
-                <p>` + replaceAll(value.content, "\n", "<br>") + `</p>
-                <div class="bottom-right-text tiny">Oprettet ` + new Date(value.creator.created_at).toLocaleString('da-DK') + `</div>
-            </div>`
-                $("#post-row").append(html);
+                addPostHtml(value);
             });
 
         } // We don't need to handle 404 here, it will simply mean that there has not been created any threads
     })
     if (Cookies.get('username')) {
-        console.log("showing stuff")
         $("#submit-row").removeClass('d-none');
-
-        $("#submit-form").submit(function(e) { 
+        $("#submit-form").submit(function (e) {
             console.log("submitted");
             const content = $("#post-input").val();
             $.ajax({
                 type: "POST",
                 url: postsUrl,
-                data: { 
+                data: {
                     content: content
                 },
-                success: function (data) {
-                    console.log(data);
-                    Cookies.set('username', username, { expires: 7 });
-                    location.href = "/"; 
+                success: function (response) {
+                    console.log(response);
+                    addPostHtml(response.data);
                 },
-                /*error: function (data) {
+                error: function (data) {
                     let response = (data.responseJSON);
-                    if (response.status == 409) {
-                        $("#signupInputUsername").after("<label id='signupInputUsername-error' class='error' for='signupInputUsername'>Brugernavnet er optaget</label>")
+                    if (response.status == 401) {
+                        alert("Du er ikke logget ind!")
                     }     
-                }*/
+                }
             });
             e.preventDefault();
         });
